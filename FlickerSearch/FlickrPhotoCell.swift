c3fe73eae34641fe3a54ad18fc8073279d9c8bca
Chantel Johnson <cjohnson@eprocessingnetwork.com>
2018-11-29 16:03:01 -0600

    tutorial part 1

---
+++ FlickerSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickerSearch
+//
+//  Created by Chantel Johnson on 11/29/18.
+//  Copyright © 2018 eProcessing Network. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
